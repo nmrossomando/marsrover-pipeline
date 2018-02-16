755b790a628f7a7dd1bc5f86b307dfe0470b2baf
Nick <nmrossomando@gmail.com>
2018-02-15 21:50:07 -0800

    Fix for small images in partial detection.

--- missions/mer/image_utils.py
+++ missions/mer/image_utils.py
@@ -14,9 +14,16 @@ import numpy as np
 # be a decent check of a partial image.
 # Return True if partial, False if otherwise
 def checkPartial(image):
-	templateImage = np.zeros((150,150,1), np.uint8) # 150 pixel square black area
-	checkCorner = image[-151:-1, -151:-1] # Bottom right 150 x 150 area of image
-	
+	# Check to see if image is large enough; our thumbnails are 64x64!
+	# If that is the case, check against the whole image - likely it's either all or nothing at this size.
+	height, width = image.shape[:2]
+	if (height < 150) or (width < 150):
+		templateImage = np.zeros((height,width,1), np.uint8)
+		checkCorner = image
+	else:
+		templateImage = np.zeros((150,150,1), np.uint8) # 150 pixel square black area
+		checkCorner = image[-151:-1, -151:-1] # Bottom right 150 x 150 area of image
+
 	# Check for full match; if every single original pixel is solid black,
 	# there's a preeeeetty good chance it's a partial.
 	if (checkCorner == templateImage).all():
